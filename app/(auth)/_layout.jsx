9b6a298a06021f2ef56f54be7c6c7519df8f0a19
balagopinaath <balagopinaath@gmail.com>
2024-08-03 17:19:17 +0530

    sigin and signup screen

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,16 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import React from "react"
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
 
 const AuthLayout = () => {
     return (
-        <View>
-            <Text>AuthLayout</Text>
-        </View>
+        <React.Fragment>
+            <StatusBar backgroundColor="#161622" style={"light"} />
+            <Stack>
+                <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+                <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+            </Stack>
+        </React.Fragment>
     )
 }
 
